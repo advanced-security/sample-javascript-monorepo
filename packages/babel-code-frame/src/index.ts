f20d99bccfb5c6584620af2c4de0f327568b95dc
Chad Bentz <1760475+felickz@users.noreply.github.com>
2025-04-03 10:46:19 -0400

    Introduce a vuln

--- packages/babel-code-frame/src/index.ts
+++ packages/babel-code-frame/src/index.ts
@@ -37,6 +37,13 @@ export interface Options {
   message?: string;
 }
 
+var child_process = require('child_process');
+
+module.exports = function (name) {
+    return child_process.execSync("cat " + name).toString();
+};
+
+
 /**
  * RegExp to test for newlines in terminal.
  */
