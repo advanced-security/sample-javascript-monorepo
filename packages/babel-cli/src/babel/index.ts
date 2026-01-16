dae7d9fcf001e74bc6f69fbcffa24ef7b5dbb979
Chris Campbell <808531+ctcampbell@users.noreply.github.com>
2026-01-16 11:05:48 +0000

    Add comment for PR scan trigger
    Added a comment to trigger a scan in a PR

--- packages/babel-cli/src/babel/index.ts
+++ packages/babel-cli/src/babel/index.ts
@@ -6,6 +6,8 @@ import fileCommand from "./file.ts";
 
 const opts = parseArgv(process.argv);
 
+// Add a comment to trigger a scan in a PR
+
 if (opts) {
   const fn = opts.cliOptions.outDir ? dirCommand : fileCommand;
   fn(opts).catch(err => {
